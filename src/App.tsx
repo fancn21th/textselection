b3c7258db3b08b1446300e7b687fb27b4ecc327a
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-21 21:32:40 +0800

    📦 NEW: two paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,34 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+    <div className="p-10">
+      <p className="mb-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus ex minus
+        illo necessitatibus magni quia unde sequi aspernatur, quasi quibusdam
+        aut obcaecati impedit ipsam quae dolor praesentium culpa labore
+        perspiciatis ipsum! Molestiae, quibusdam veniam. Consectetur doloremque,
+        vel praesentium fugit excepturi iure necessitatibus labore commodi! Quo
+        quae doloribus quas quos quam nemo fugit. Vel odit, laborum, harum
+        maiores provident doloremque ipsam saepe asperiores tenetur natus
+        voluptatibus vero magni quam atque! Architecto et ipsa fugit beatae,
+        voluptatibus quis impedit deserunt assumenda, illum, eos repellendus
+        tempora velit nobis! Fugit nemo nobis nisi quisquam. Quas maxime, quo
+        mollitia a reiciendis perferendis at libero praesentium.
+      </p>
+      <p className="mb-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus ex minus
+        illo necessitatibus magni quia unde sequi aspernatur, quasi quibusdam
+        aut obcaecati impedit ipsam quae dolor praesentium culpa labore
+        perspiciatis ipsum! Molestiae, quibusdam veniam. Consectetur doloremque,
+        vel praesentium fugit excepturi iure necessitatibus labore commodi! Quo
+        quae doloribus quas quos quam nemo fugit. Vel odit, laborum, harum
+        maiores provident doloremque ipsam saepe asperiores tenetur natus
+        voluptatibus vero magni quam atque! Architecto et ipsa fugit beatae,
+        voluptatibus quis impedit deserunt assumenda, illum, eos repellendus
+        tempora velit nobis! Fugit nemo nobis nisi quisquam. Quas maxime, quo
+        mollitia a reiciendis perferendis at libero praesentium.
+      </p>
+    </div>
   );
 }
 
