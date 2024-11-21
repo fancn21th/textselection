65bd27e1e0e76230085ce4b0a5897ac9cae194bd
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-21 21:29:27 +0800

    📦 NEW: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+  );
 }
 
 export default App;
