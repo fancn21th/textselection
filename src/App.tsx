c8402f2f98504fa7caca4799e4c8817ad43b4bc2
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-22 16:30:17 +0800

    📦 NEW: selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, Fragment } from "react";
+import { useState, Fragment } from "react";
+import ActionPanel from "./components/ActionPanel";
 import "./App.css";
 
 const text = `
@@ -7,34 +8,50 @@ const text = `
 
 function App() {
   const [content] = useState(text.split(" "));
-  const [cursorIndex, setCursorIndex] = useState(0);
+  const [startCursorIndex, setStartCursorIndex] = useState(0);
+  const [endCursorIndex, setEndCursorIndex] = useState(content.length - 1);
 
   const go = (event: React.MouseEvent<HTMLDivElement>) => {
     if (event.button === 0)
       // Decrement cursor position (loop back at start)
-      setCursorIndex((prev) => (prev - 1 + content.length) % content.length);
+      setStartCursorIndex(
+        (prev) => (prev - 1 + content.length) % content.length
+      );
     if (event.button === 2)
-      setCursorIndex((prev) => (prev + 1) % content.length);
+      setStartCursorIndex((prev) => (prev + 1) % content.length);
   };
 
   return (
     <>
-      <h2>鼠标左键点击向左，右键点击向右，当前位置为：{cursorIndex}</h2>
-      <div
-        className="p-10"
-        onMouseDown={go}
-        onContextMenu={(e) => e.preventDefault()}
-      >
+      <div className="p-10">
+        <h3>段落</h3>
+        <hr className="my-2" />
         <p className="mb-2">
           {content.map((word, idx) => (
             <Fragment key={idx}>
-              {idx === cursorIndex ? (
-                <span className="text-red-500">&#x2588;</span>
+              {idx === startCursorIndex ? (
+                <span className="border-r-4 border-r-red-500 pr-1">🙉</span>
+              ) : null}
+              {idx === endCursorIndex ? (
+                <span className="border-l-4 border-l-red-500 pl-1">🙈</span>
               ) : null}
               {word}{" "}
             </Fragment>
           ))}
         </p>
+        <h3>选中段落</h3> <hr className="my-2" />
+        <p className="mb-2">
+          {content.map((word, idx) => {
+            if (idx < startCursorIndex) return;
+            if (idx > endCursorIndex) return;
+            return <Fragment key={idx}>{word} </Fragment>;
+          })}
+        </p>
+        <ActionPanel
+          onClick={(type, isAdd) => {
+            console.log(type, isAdd);
+          }}
+        />
       </div>
     </>
   );
