68b264225343ee8725a3fca81c8f3b4c15591bd6
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-28 22:45:30 +0800

    📦 NEW: mark the overlapped

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,62 @@ function App() {
   const [cursors] = useState([
     { s: 100, e: 300 },
     { s: 200, e: 500 },
-    { s: 500, e: 600 },
+    { s: 400, e: 600 },
     { s: 600, e: 700 },
   ]);
   const [resolvedCursors, setResolvedCursors] = useState<
-    { s: number; e: number; index?: number }[]
+    {
+      s: number;
+      e: number;
+      index?: number;
+      tailOverlapped?: number;
+      headOverlapped?: number;
+    }[]
   >([]);
 
   useEffect(() => {
     // 排序
     const sorted = cursors.slice().sort((a, b) => a.s - b.s);
-    const indexed = sorted.map((cursor, index) => ({ ...cursor, index }));
+    const indexed = sorted.map(
+      (
+        cursor,
+        index
+      ): {
+        s: number;
+        e: number;
+        index: number;
+        tailOverlapped: number;
+        headOverlapped: number;
+      } => ({
+        ...cursor,
+        index,
+        tailOverlapped: -1,
+        headOverlapped: -1,
+      })
+    );
+
+    // 标记重叠
+    let last = indexed[0];
+    for (let i = 1; i < indexed.length; i++) {
+      const current = indexed[i];
+      // 如果 current.s < last.e 说明有重叠
+      if (current.s < last.e) {
+        last.tailOverlapped = current.s;
+        current.headOverlapped = last.e;
+      }
+      last = current;
+    }
 
     console.log({ indexed });
 
     // 填充空隙
     const filledCursors = [];
     let lastEnd = 0;
-    for (const { s, e, index } of indexed) {
+    for (const { s, e, index, headOverlapped, tailOverlapped } of indexed) {
       if (s > lastEnd) {
         filledCursors.push({ s: lastEnd, e: s });
       }
-      filledCursors.push({ s, e, index });
+      filledCursors.push({ s, e, index, headOverlapped, tailOverlapped });
       lastEnd = e;
     }
     if (lastEnd < content.length) {
