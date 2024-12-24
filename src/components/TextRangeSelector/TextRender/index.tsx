8c8e7f14a6b0c512e50249aa55f8fdde124ac055
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-12-24 22:52:19 +0800

    👌 IMPROVE: txt transparent

--- src/components/TextRangeSelector/TextRender/index.tsx
+++ src/components/TextRangeSelector/TextRender/index.tsx
@@ -68,6 +68,7 @@ function Text() {
                       <span
                         key={_index}
                         className={clsx(
+                          "text-transparent",
                           part.isEven && !part.isFill && "bg-red-300",
                           part.isOdd && !part.isFill && "bg-green-300",
                           part.isFill && "bg-transparent"
