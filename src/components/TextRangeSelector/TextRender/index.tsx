240f465e265b45e5ed13720f500821fb1afce5c9
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-12-24 10:03:21 +0800

    👌 IMPROVE: ts error

--- src/components/TextRangeSelector/TextRender/index.tsx
+++ src/components/TextRangeSelector/TextRender/index.tsx
@@ -1,7 +1,6 @@
 import { useContext, useMemo, useState } from "react";
 import { TextRangeSelectionContext } from "../context/TextRangeSelectionContext";
 import type { TextRangeSelectionContextType } from "../context/TextRangeSelectionContext";
-import clsx from "clsx";
 import { FixedSizeList as List } from "react-window";
 import { createPortal } from "react-dom";
 
@@ -33,7 +32,13 @@ function Text() {
         itemCount={chunks.length} // 总块数
         itemSize={30} // 每行高度
         width="100%" // 宽度适应父容器
-        onItemsRendered={({ visibleStartIndex, visibleStopIndex }) => {
+        onItemsRendered={({
+          visibleStartIndex,
+          visibleStopIndex,
+        }: {
+          visibleStartIndex: number;
+          visibleStopIndex: number;
+        }) => {
           // 更新当前可视区域
           setVisibleRange({
             startIndex: visibleStartIndex,
@@ -41,7 +46,7 @@ function Text() {
           });
         }}
       >
-        {({ index, style }) => (
+        {({ index, style }: { index: number; style: React.CSSProperties }) => (
           <div style={style}>
             <span>{chunks[index]}</span>
           </div>
@@ -49,7 +54,7 @@ function Text() {
       </List>
       {/* debugger */}
       {createPortal(
-        <div className="absolute bottom-0 left-0 bg-gray-100 p-2 text-sm">
+        <div className="absolute top-0 left-0 bg-gray-100 p-2 text-sm">
           <p>Visible Range:</p>
           <p>Start: {visibleRange.startIndex}</p>
           <p>End: {visibleRange.endIndex}</p>
