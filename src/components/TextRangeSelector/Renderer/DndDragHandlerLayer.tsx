6ebfe3b3f91bfec36d35ad1375f6f5b1aed9721d
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-01-14 15:23:41 +0800

    👌 IMPROVE: DndDragHandler not re-render

--- src/components/TextRangeSelector/Renderer/DndDragHandlerLayer.tsx
+++ src/components/TextRangeSelector/Renderer/DndDragHandlerLayer.tsx
@@ -1,31 +1,62 @@
-import { useContext } from "react";
-import { DndContext } from "../context/DndContext";
+import { useContext, useMemo } from "react";
+import { CoordsWithPos, DndContext } from "../context/DndContext";
 import clsx from "clsx";
+import { Cursor } from "./components/Cursor";
+import React from "react";
+
+const DndDragHandler = React.memo(
+  ({
+    startCoords,
+    endCoords,
+  }: {
+    startCoords: CoordsWithPos;
+    endCoords: CoordsWithPos;
+  }) => {
+    console.log({ DndDragHandler });
+
+    return (
+      <div className="w-[100%] h-[600px] bg-transparent absolute left-0 top-0 pointer-events-none">
+        {startCoords.pos.index !== -1 && (
+          <div
+            className={clsx(
+              "font-extrabold text-pink-500",
+              "absolute z-50 cursor-move pointer-events-auto"
+            )}
+            style={{
+              left: `${startCoords.left}px`,
+              top: `${startCoords.top}px`,
+            }}
+          >
+            <Cursor pos={startCoords.pos} />
+          </div>
+        )}
+        {endCoords.pos.index !== -1 && (
+          <div
+            className={clsx(
+              "font-extrabold text-pink-500",
+              "absolute z-50 cursor-move pointer-events-auto"
+            )}
+            style={{ left: `${endCoords.left}px`, top: `${endCoords.top}px` }}
+          >
+            <Cursor pos={endCoords.pos} />
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 const DndDragHandlerLayer = () => {
   const { startCoords, endCoords } = useContext(DndContext);
 
+  const memoizedStartCoords = useMemo(() => startCoords, [startCoords]);
+  const memoizedEndCoords = useMemo(() => endCoords, [endCoords]);
+
   return (
-    <div className="w-[100%] h-[600px] bg-transparent absolute left-0 top-0 pointer-events-none">
-      <span
-        className={clsx(
-          "font-extrabold text-pink-500",
-          "absolute z-50 cursor-move pointer-events-auto"
-        )}
-        style={{ left: `${startCoords.left}px`, top: `${startCoords.top}px` }}
-      >
-        X
-      </span>
-      <span
-        className={clsx(
-          "font-extrabold text-pink-500",
-          "absolute z-50 cursor-move pointer-events-auto"
-        )}
-        style={{ left: `${endCoords.left}px`, top: `${endCoords.top}px` }}
-      >
-        X
-      </span>
-    </div>
+    <DndDragHandler
+      startCoords={memoizedStartCoords}
+      endCoords={memoizedEndCoords}
+    />
   );
 };
 
